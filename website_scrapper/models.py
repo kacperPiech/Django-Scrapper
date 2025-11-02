a1f8ce5164b5f0446a57871c8401b6ed2daedf54
kacperPiech <kacper.piech@o2.pl>
2025-11-02 16:39:04 +0100

    feat/add_website_content_class_in_models

--- website_scrapper/models.py
+++ website_scrapper/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Website_Content(models.Model):
+    title = models.CharField(max_length = 150)
+    website_html_content = models.TextField()
+    website_plain_content = models.TextField()
+    url = models.URLField(unique = True)
+    publication_date = models.DateTimeField()
+    
+    def __str__(self):
+        return f"{self.title} at {self.publication_date}"
+    
+    
+
