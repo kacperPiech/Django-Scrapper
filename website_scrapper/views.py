44825574d984b5deb841af02e3a1dd1f13b8337a
kacperPiech <kacper.piech@o2.pl>
2025-11-03 09:23:30 +0100

    feat/add_views_and_urls

--- website_scrapper/views.py
+++ website_scrapper/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Website_Content
+from .serializers import WebsiteContentSerializer
 
-# Create your views here.
+class WebsiteContentViewSet(viewsets.ModelViewSet):
+    queryset = Website_Content.objects.all().order_by('-publication_date')
+    serializer_class = WebsiteContentSerializer
+
